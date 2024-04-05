1fd649c7055dc9549b8ba276fdb07ec9f0bfc68b
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2024-04-05 18:46:52 +0600

    feat(sahred): header and footer component

--- library/shared/src/lib/components/footer/footer.component.ts
+++ library/shared/src/lib/components/footer/footer.component.ts
@@ -6,6 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {}
