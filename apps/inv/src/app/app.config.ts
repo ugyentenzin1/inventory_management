994afcb3a3ee3d35055ce4970e39a448a53ff21f
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2024-04-12 16:52:53 +0600

    feat(auth): auth routing and component creation done

--- apps/inv/src/app/app.config.ts
+++ apps/inv/src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 
 import {ENVIROMENT} from '../../../../enviroment'
+import { coreRouets } from './core/core.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(coreRouets),
     importProvidersFrom([
       provideFirebaseApp(() => initializeApp(ENVIROMENT.firebaseConfig)),
     ])
